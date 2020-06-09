efc0b62c202300a3bd5eec594435fea3440da104
hazae41 <hazae41@gmail.com>
2020-06-10 00:41:47 +0200

    tsc

--- start.ts
+++ start.ts
@@ -11,8 +11,8 @@ import {
 import { Banhammer } from "./plugins/banhammer@1.0.ts";
 import { MOTD } from "./plugins/motd@1.0.ts";
 
-const target = { hostname: "127.0.0.1", port: 19134 };
-const handlers = [new Handler(19132, target)];
+const target = { hostname: "127.0.0.1", port: 19132 };
+const handlers = [new Handler(19134, target)];
 const saurus = new Saurus({ handlers });
 
 const minecraft = new Minecraft(["minecraft/bedrock_server.exe"]);
